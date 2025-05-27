011c89ae535621e22ae20271b5a9b09dcc168df7
aoweichen <2557780575@qq.com>
2025-05-28 00:48:06 +0800

    更新 CMakeLists.txt 去除动态库部分，暂时不考虑使用动态库

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 // Created by aoweichen on 2025/5/27.
 //
 
+#include <openblas/cblas.h>  // 或完整路径 #include "include/cblas.h"
 #include <iostream>
 
-int main(){
-    std::cout<<"Hello world!"<<std::endl;
+int main() {
+    double x[] = {1.0, 2.0, 3.0};
+    double y[] = {4.0, 5.0, 6.0};
+    double dot = cblas_ddot(3, x, 1, y, 1);
+    std::cout << "Dot product: " << dot << std::endl;
     return 0;
 }

019db1253d0b7813450cb32ba324b4f5ac3b1cde
aoweichen <2557780575@qq.com>
2025-05-28 01:23:55 +0800

    删除没必要的代码文件

---
+++ include/runtime/pnnx/ir.hpp
@@ -0,0 +1,8 @@
+//
+// Created by aoweichen on 2025/5/28.
+//
+
+#ifndef IR_HPP
+#define IR_HPP
+
+#endif //IR_HPP
---
+++ include/runtime/pnnx/storezip.hpp
@@ -0,0 +1,8 @@
+//
+// Created by aoweichen on 2025/5/28.
+//
+
+#ifndef STOREZIP_HPP
+#define STOREZIP_HPP
+
+#endif //STOREZIP_HPP

efe9e7212832a764e121cade4028fc77afb4a73e
aoweichen <2557780575@qq.com>
2025-05-29 00:08:28 +0800

    添加宏来处理不同环境下的对齐

--- include/runtime/pnnx/storezip.hpp
+++ include/runtime/pnnx/storezip.hpp
@@ -5,4 +5,16 @@
 #ifndef STOREZIP_HPP
 #define STOREZIP_HPP
 
+
+namespace pnnx {
+#ifdef _MSC_VER
+#define PACK(__Declaration__) __pragma(pack(push, 1)) __Declaration__ __pragma(pack(pop))
+#else
+#define PACK(__Declaration__) __Declaration__ __attribute__((__packed__))
+#endif
+}
+
+
+
+
 #endif //STOREZIP_HPP

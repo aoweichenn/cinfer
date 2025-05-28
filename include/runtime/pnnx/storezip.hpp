6dd754c34ad92b20f01653854323975e1fc2e39a
aoweichen <2557780575@qq.com>
2025-05-29 00:23:57 +0800

    删除宏来处理不同环境下的对齐，编写处理pnnx存储模型和读取模型的类

--- include/runtime/pnnx/storezip.hpp
+++ include/runtime/pnnx/storezip.hpp
@@ -4,17 +4,55 @@
 
 #ifndef STOREZIP_HPP
 #define STOREZIP_HPP
-
+#include <cstdint>
+#include <map>
+#include <string>
+#include <vector>
 
 namespace pnnx {
-#ifdef _MSC_VER
-#define PACK(__Declaration__) __pragma(pack(push, 1)) __Declaration__ __pragma(pack(pop))
-#else
-#define PACK(__Declaration__) __Declaration__ __attribute__((__packed__))
-#endif
+    struct StoreZipMetaOfReader {
+        uint64_t offset;
+        uint64_t size;
+    };
+
+    struct StoreZipMetaOfWriter {
+        std::string name;
+        uint64_t lfh_offset;
+        uint32_t crc32;
+        uint64_t size;
+    };
 }
 
+namespace pnnx {
+    class StoreZipReader {
+    public:
+        StoreZipReader();
 
+        ~StoreZipReader();
 
+    private:
+        FILE *fp;
+        std::map<std::string, StoreZipMetaOfReader> filemetas;
+    };
+}
+
+namespace pnnx {
+    class StoreZipWriter {
+    public:
+        StoreZipWriter();
+
+        ~StoreZipWriter();
+
+        int open(const std::string &path);
+
+        int write_file(const std::string &name, const char *data, uint64_t size);
+
+        int close();
+
+    private:
+        FILE *fp;
+        std::vector<StoreZipMetaOfWriter> filemetas;
+    };
+}
 
 #endif //STOREZIP_HPP
